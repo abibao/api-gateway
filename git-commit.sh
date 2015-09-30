3ecabc116ad7144090afa3bca4bc690052053b55
Team@Abibao <team@abibao.com>
2015-09-30 08:47:18 +0000

    only commit if tests are 100% done

--- git-commit.sh
+++ git-commit.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+# run tests
 npm test
 
-git add -i
-git commit -m "$2"
-git push origin $1
+# if tests are not 100% ok, then no need to commit...
+if [ $? -eq 0 ]
+  then
+    git add -i
+    git commit -m "$2"
+    git push origin $1
+fi
--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 var CRUDUser = {
   db: process.env.ABIBAO_MONGODB__DATABASE_CONNECTION,
