a70621bc651d5fc5b8028447a68f44f13042a56e
Gilles Perreymond <gperreymond@gmail.com>
2016-05-16 07:24:27 +0200

    [analytics] add auto insert answer analytics

--- src/domain/commands/individualSurveyAnswerCommand.js
+++ src/domain/commands/individualSurveyAnswerCommand.js
@@ -22,6 +22,34 @@ module.exports = function (payload) {
           waterfall.survey.answers[payload.label] = survey.formatAnswer(survey.type, payload.answer)
           waterfall.survey.complete = _.keys(waterfall.survey.answers).length === waterfall.items.length
           return self.execute('command', 'surveyUpdateCommand', waterfall.survey).then(function (updated) {
+            // event for analytics
+            var regex = /^(urn:abibao:database:)/
+            var _answer
+            if (_.isArray(payload.answer)) {
+              _.map(payload.answer, function (item) {
+                if (regex.exec(item) !== null) {
+                  _answer = self.getIDfromURN(item)
+                } else {
+                  _answer = item
+                }
+                global.ABIBAO.services.bus.send(global.ABIBAO.events.BusEvent.BUS_EVENT_ANALYTICS_COMPUTE_ANSWER, {
+                  survey: self.getIDfromURN(survey.urn),
+                  label: payload.label,
+                  answer: _answer
+                })
+              })
+              if (regex.exec(payload.answer) !== null) {
+                _answer = self.getIDfromURN(payload.answer)
+              } else {
+                _answer = payload.answer
+              }
+              global.ABIBAO.services.bus.send(global.ABIBAO.events.BusEvent.BUS_EVENT_ANALYTICS_COMPUTE_ANSWER, {
+                survey: self.getIDfromURN(survey.urn),
+                label: payload.label,
+                answer: _answer
+              })
+            }
+            // auto affectation ?
             if (waterfall.survey.isAbibao === true && waterfall.survey.complete === true) {
               global.ABIBAO.services.bus.send(global.ABIBAO.events.BusEvent.BUS_EVENT_EMAIL_ABIBAO_AFFECT_CAMPAIGNS_AUTO, {
                 urnIndividual: waterfall.survey.urnIndividual,
