4d8f15e31c6580c8ec1b9dfcef7cae21a018c522
Gilles Perreymond <gperreymond@gmail.com>
2016-06-21 23:08:35 +0200

    [hotfix] put good token for codacy coverage

--- npm-scripts/test-all.sh
+++ npm-scripts/test-all.sh
@@ -2,7 +2,7 @@
 
 clear
 
-export CODACY_PROJECT_TOKEN=81efd5f3bef1435188fa264d5dc98475
+export CODACY_PROJECT_TOKEN=3862e173a5284d0984ee3bb50d6a8f24
 rm -rf coverage
 node_modules/.bin/istanbul cover node_modules/.bin/_mocha --report lcovonly -- --timeout 15000 -R spec
 cat coverage/lcov.info | node_modules/.bin/codacy-coverage
