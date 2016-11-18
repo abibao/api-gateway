83f49a35971cf7c47588e707c2d1152822c2f178
Gilles Perreymond <gperreymond@viadeoteam.com>
2016-11-18 10:46:14 +0100

    docker-compose with logstash and elasticsearch

--- npm-scripts/setup-deve.sh
+++ npm-scripts/setup-deve.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 set -e
 
+docker-compose up -d --build
+
 node_modules/.bin/_mocha --timeout 5000 --use_strict --recursive test/rethinkdb-structure.test.js
 node_modules/.bin/_mocha --timeout 5000 --use_strict --recursive test/mysql-structure.test.js
