bb9a00358e429d8d36d62492287408ec4759b47b
gperreymond <gperreymond@gmail.com>
2016-11-27 21:50:45 +0100

    smf caldera tools

--- src/tools/helpers/smf_caldera.js
+++ src/tools/helpers/smf_caldera.js
@@ -8,6 +8,7 @@ const nconf = require('nconf')
 nconf.argv().env().file({ file: 'nconf-deve.json' })
 
 // read csv file content
+const async = require('async')
 const path = require('path')
 const fs = require('fs')
 const file = path.resolve(__dirname, 'candidatures.csv')
@@ -34,70 +35,87 @@ csv
   .on('end', () => {
     console.log('candidatures: ', candidatures.length)
     candidatures.shift()
-    // download all images
+    // *******************************
+    // ***** download all images *****
+    // *******************************
     let images = []
     _.map(candidatures, (candidature) => {
+      const parts = candidature[10].split('/')
       images.push({
-        title: candidature[1].trim(),
+        title: parts[parts.length - 1],
         uri: candidature[10].trim()
       })
     })
-    Promise.all(images.map((image) => {
-      download(image.uri).then(data => {
-        fs.writeFileSync('dist' + image.title, data)
-      })
-    })
-    .then(() => {
-      console.log('done')
-    })
-    // action
-    /* const candidature = candidatures[0]
-    const payload = {
-      title: candidature[1].trim(),
-      excerpt: candidature[8].trim(),
-      content: candidature[9].trim(),
-      status: 'publish',
-      'ping_status': 'closed',
-      'comment_status': 'closed',
-      categories: [4]
-    }
-    const mediaURI = candidature[10]
-    Promise.all([mediaURI].map(x => download(x, 'dist'))).then(() => {
-      wp.media('dist')
-      // Specify a path to the file you want to upload
-      .file()
-      .create({
-        title: 'My awesome image',
-        alt_text: 'an image of something awesome',
-        caption: 'This is the caption text',
-        description: 'More explanatory information'
-      })
-      .then((response) => {
-        // Your media is now uploaded: let's associate it with a post
-        var newImageId = response.id
-        return wp.media().id(newImageId).update({
-          post: associatedPostId
+    async.eachLimit(images, 1, (image, next) => {
+      return next()
+      if (image.uri === '') {
+        return next()
+      }
+      console.log(image.uri, 'start')
+      download(image.uri)
+        .then(data => {
+          let target = path.resolve(__dirname, '../.cache/images', image.title)
+          fs.writeFileSync(target, data)
+          console.log(target, 'downloaded')
+          next()
         })
-      })
-      .then((response) => {
-        console.log('Media ID #' + response.id)
-        console.log('is now associated with Post ID #' + response.post)
-      })
-    }) */
-    // step 1: posts()
-    /* wp.posts().filter({title: payload.title}).get()
-      .then((posts) => {
-        if (posts.length === 0) {
-          return wp.posts().create(payload)
-        } else {
-          return wp.posts().id(posts[0].id).update(payload)
-        }
-      })
-      .then((result) => {
-        console.log('done')
-      })
-      .catch((error) => {
-        console.log(error)
-      }) */
-    // step 2: media()
+        .catch((error) => {
+          console.log(error)
+          next()
+        })
+    }, () => {
+      // *******************************
+      // ***** media posts         *****
+      // *******************************
+      const candidature = candidatures[1]
+      const parts = candidature[10].split('/')
+      console.log('media posts', parts)
+      let source = path.resolve(__dirname, '../.cache/images', parts[parts.length - 1])
+      wp.media()
+        .filter({title: candidature[1].trim()})
+        .then((result) => {
+          if (result.length === 0) {
+            return wp.media()
+              .file(source)
+              .create({
+                title: candidature[1].trim(),
+                alt_text: candidature[1].trim(),
+                caption: candidature[1].trim(),
+                description: candidature[1].trim()
+              })
+          } else {
+            return result[0]
+          }
+        })
+        .then((result) => {
+          // *******************************
+          // ***** create/update posts *****
+          // *******************************
+          console.log('create/update posts')
+          const payload = {
+            title: candidature[1].trim(),
+            excerpt: candidature[8].trim(),
+            content: candidature[9].trim(),
+            status: 'publish',
+            'ping_status': 'closed',
+            'comment_status': 'closed',
+            'featured_media': result.id,
+            categories: [4]
+          }
+          wp.posts().filter({title: payload.title}).get()
+            .then((posts) => {
+              if (posts.length === 0) {
+                return wp.posts().create(payload)
+              } else {
+                return wp.posts().id(posts[0].id).update(payload)
+              }
+            })
+            .then((result) => {
+              console.log('done')
+            })
+            .catch((error) => {
+              console.log(error)
+            })
+        })
+    })
   })
