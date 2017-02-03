8b396e6222bc184daabfe51f4ee79a97c1634e50
Gilles Perreymond <gperreymond@viadeoteam.com>
2017-02-03 16:32:42 +0100

    rewrite the create of mysql structure for tests

--- test-v1/mysql-structure.test.js
+++ test-v1/mysql-structure.test.js
@@ -4,52 +4,101 @@
 var nconf = require('nconf')
 nconf.argv().env().file({ file: 'nconf-deve.json' })
 
-var AnswerModel = require('../src/domain/models/mysql/AnswerModel')
-var UserModel = require('../src/domain/models/mysql/UserModel')
-var VoteSMFModel = require('../src/domain/models/mysql/VoteSMFModel')
-var SendgridBounceModel = require('../src/domain/models/mysql/SendgridBounceModel')
-
 describe('mysql structure', function () {
-  it('should create database analytics', function (done) {
-    var database = nconf.get('ABIBAO_API_GATEWAY_SERVER_MYSQL_DATABASE')
-    var knex = require('../src/connections/knex')('EMPTY')
-    knex.raw('CREATE SCHEMA IF NOT EXISTS `' + database + '` DEFAULT CHARACTER SET utf8;')
-      .then(() => {
-        done()
-      })
-      .catch(done)
+  it('should create database analytics.answers', function (done) {
+    var options = {
+      client: 'mysql',
+      connection: {
+        host: nconf.get('MYSQL_ENV_DOCKERCLOUD_SERVICE_FQDN'),
+        port: nconf.get('MYSQL_PORT_3306_TCP_PORT'),
+        user: nconf.get('ABIBAO_API_GATEWAY_SERVER_MYSQL_USER'),
+        password: nconf.get('MYSQL_ENV_MYSQL_ROOT_PASSWORD'),
+        database: nconf.get('ABIBAO_API_GATEWAY_DATABASES_MYSQSL_ANALYTICS')
+      },
+      debug: false
+    }
+    var knex = require('knex')(options)
+    knex.schema.createTableIfNotExists('answers', function (table) {
+      table.increments('id')
+      table.string('email')
+      table.string('campaign_id')
+      table.string('charity_id')
+      table.string('campaign_name')
+      table.string('charity_name')
+      table.string('question')
+      table.string('answer')
+      table.string('answer_text')
+      table.timestamp('createdAt').defaultTo(knex.fn.now())
+    }).then(() => {
+      knex.destroy()
+      done()
+    }).catch((error) => {
+      knex.destroy()
+      done(error)
+    })
   })
-  it('should create table analytics.answers if not exists', function (done) {
-    AnswerModel()
-      .then(() => {
-        done()
-      }).catch(done)
+  it('should create database analytics.users', function (done) {
+    var options = {
+      client: 'mysql',
+      connection: {
+        host: nconf.get('MYSQL_ENV_DOCKERCLOUD_SERVICE_FQDN'),
+        port: nconf.get('MYSQL_PORT_3306_TCP_PORT'),
+        user: nconf.get('ABIBAO_API_GATEWAY_SERVER_MYSQL_USER'),
+        password: nconf.get('MYSQL_ENV_MYSQL_ROOT_PASSWORD'),
+        database: nconf.get('ABIBAO_API_GATEWAY_DATABASES_MYSQSL_ANALYTICS')
+      },
+      debug: false
+    }
+    var knex = require('knex')(options)
+    knex.schema.createTableIfNotExists('users', function (table) {
+      table.increments('id')
+      table.string('email')
+      table.string('charity')
+      table.string('registeredCharity')
+      table.integer('age')
+      table.string('csp')
+      table.string('department')
+      table.string('gender')
+      table.timestamp('modifiedAt')
+      table.timestamp('createdAt').defaultTo(knex.fn.now())
+    }).then(() => {
+      knex.destroy()
+      done()
+    }).catch((error) => {
+      knex.destroy()
+      done(error)
+    })
   })
-  it('should create table analytics.users if not exists', function (done) {
-    UserModel()
-      .then(() => {
-        done()
-      }).catch(done)
-  })
-  it('should create table analytics.smf_votes if not exists', function (done) {
-    VoteSMFModel()
-      .then(() => {
-        done()
-      }).catch(done)
-  })
-  it('should create database sendgrid', function (done) {
-    var database = 'sendgrid'
-    var knex = require('../src/connections/knex')('EMPTY')
-    knex.raw('CREATE SCHEMA IF NOT EXISTS `' + database + '` DEFAULT CHARACTER SET utf8;')
-      .then(() => {
-        done()
-      })
-      .catch(done)
-  })
-  it('should create table sendgrid.bounces if not exists', function (done) {
-    SendgridBounceModel()
-      .then(() => {
-        done()
-      }).catch(done)
+  it('should create database analytics.smf_votes', function (done) {
+    var options = {
+      client: 'mysql',
+      connection: {
+        host: nconf.get('MYSQL_ENV_DOCKERCLOUD_SERVICE_FQDN'),
+        port: nconf.get('MYSQL_PORT_3306_TCP_PORT'),
+        user: nconf.get('ABIBAO_API_GATEWAY_SERVER_MYSQL_USER'),
+        password: nconf.get('MYSQL_ENV_MYSQL_ROOT_PASSWORD'),
+        database: nconf.get('ABIBAO_API_GATEWAY_DATABASES_MYSQSL_ANALYTICS')
+      },
+      debug: false
+    }
+    var knex = require('knex')(options)
+    knex.schema.createTableIfNotExists('smf_votes', function (table) {
+      table.increments('id')
+      table.string('email')
+      table.string('charity')
+      table.string('registeredCharity')
+      table.integer('age')
+      table.string('csp')
+      table.string('department')
+      table.string('gender')
+      table.timestamp('modifiedAt')
+      table.timestamp('createdAt').defaultTo(knex.fn.now())
+    }).then(() => {
+      knex.destroy()
+      done()
+    }).catch((error) => {
+      knex.destroy()
+      done(error)
+    })
   })
 })
